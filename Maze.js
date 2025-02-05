2dcebcc0defea50e06a010e3aa8d16ff1f9ade5b
AJones6000 <alexjones4053@gmail.com>
2025-02-05 08:21:29 -0500

    make it work over the green thing

---
+++ Maze.js
@@ -0,0 +1,202 @@
+//Team Members: Robert Tanase and Alex Jones
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-F28B
+
+//begin program
+async function startProgram() {
+
+//Start and Say Start
+//Bolt will say Start across the LED's and Say start out loud
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+	await speak("Start", true);
+	await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true);
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+	await roll(0, 70, 2);
+	await delay(1);
+	setMainLed({ r: 0, g: 255, b: 0 });
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+	await delay(1);
+ 	await spin(90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 37, 3);
+	await delay(1);
+	await spin(90,1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 2.2);
+	await delay(1);
+	await Sound.Game.Coin.play(true);
+	await delay(1);
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+
+ 	await spin(44, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 2.4);
+	await delay(1);
+	setMainLed({ r: 255, g: 0, b: 0 });
+	await delay(1);
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 39, 1.1);
+	await delay(1);
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 2.4);
+	await delay(1);
+	await Sound.Animal.Cat.play(true);
+	await delay(1);
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+	await spin(75, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 38, 1.5);
+	await delay(1);
+	await spin(60, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 41, 0.9);
+	await delay(1);
+	setMainLed({ r: 159, g: 43, b: 104 });
+
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+	await delay(1);
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 1.4);
+	await delay(1);
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 40, 1.4);
+	await delay(1);
+	await Sound.Animal.Bear.play(true);
+
+
+
+//Blue Checkpoint
+//LED's turn Blue
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+	await delay(1);
+	await spin(-45, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 2);
+	setMainLed({ r: 0, g: 0, b: 255 });
+//Finished and say finished
+//Bolt will say Finished across the LED's and Say Finished out loud
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+
+await delay(1);
+await spin(45, 1);
+await delay(1);
+await roll((getHeading() + 0), 40, 2.5);
+await speak("Finished", true);
+await scrollMatrixText('finished', { r: 66, g: 56, b: 255 }, 30, true);
+
+
+//End Program
+}
+/*
+//Team Members: Robert Tanase and Alex Jones
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-F28B
+
+//begin program
+async function startProgram() {
+
+//Start and Say Start
+//Bolt will say Start across the LED's and Say start out loud
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+	await speak("Start", true);
+	await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true);
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+	await roll(0, 70, 2);
+	await delay(1);
+	setMainLed({ r: 0, g: 255, b: 0 });
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+	await delay(1);
+ 	await spin(90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 37, 3);
+	await delay(1);
+	await spin(90,1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 2.2);
+	await delay(1);
+	await Sound.Game.Coin.play(true);
+	await delay(1);
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+
+ 	await spin(44, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 35, 2.4);
+	await delay(1);
+	setMainLed({ r: 255, g: 0, b: 0 });
+	await delay(1);
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 39, 1.1);
+	await delay(1);
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 90, 1.7);
+	await delay(1);
+	await spin(-45,1);
+	await roll((getHeading() + 0), 35, 2);
+	*/
